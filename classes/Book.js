0d24f8b28c876c411f5659bddbefc6c6c9a90342
DavidGonzalez916 <dagonzalez@deloitte.com>
2024-03-12 15:59:57 -0400

    Book and Media Complete and passed Tests

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,27 @@
-// import the Media class:
+const Media = require('./Media')
 
-// create your Book class:
+class Book extends Media{
+    constructor(title, year, genre, author, numPages, rating){
+        super(title, year, genre);
+        this.rating = rating;
+        this.author = author;
+        this.numPages = numPages;
+    }
+    summary(){
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+    static highestRating(books){
+        let max = 0;
+        let result;
+        for(let i = 0; i < books.length; i++){
+            if(max < books[i].rating){
+                max = books[i].rating;
+                result = books[i];
+            } 
+        }
+        return result;
+    }
+}
 
 // don't change below
 module.exports = Book;
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Import classes here to console.log and debug
+const Media = require('./classes/Media.js');
+const Book = require('./classes/Book.js');
+
+function main(){
+    const book1 = new Book('To Kill a Mockingbird', 1960, 'Fiction', 'Harper Lee', 281, 4.7);
+Media.totalMediaCount; // 1
+const book2 = new Book('The Bluest Eye', 1970, 'Fiction', 'Toni Morrison', 206, 4.6);
+Media.totalMediaCount; // 2
+book1.summary(); // "Title: To Kill a Mockingbird, Author: Harper Lee, Year: 1960, Page Count: 281, Genre: Fiction, Rating: 4.4"
+Book.highestRating([book1, book2]); // Returns book2
+}
+
+main();
